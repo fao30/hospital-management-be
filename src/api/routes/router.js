5368393d77c476ed6a64f31d40b484a0e9eaf8a0
fao30 <fakhrularifin@windowslive.com>
2024-01-03 20:04:50 +0700

    Login register router

--- src/api/routes/router.js
+++ src/api/routes/router.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 const RolesRoutes = require("./listRoutes/rolesRoutes");
 const CountriesRoutes = require("./listRoutes/countriesRoutes");
 const PaymentStatusesRoutes = require("./listRoutes/paymentStatusesRoutes");
+const LoginRoutes = require("./registerLogin/LoginRouter");
+const RegisterRoutes = require("./registerLogin/RegisterRouter");
 
+router.use("/api/login", LoginRoutes);
+router.use("/api/register", RegisterRoutes);
 router.use("/api/roles", RolesRoutes);
 router.use("/api/countries", CountriesRoutes);
 router.use("/api/paymentStatuses", PaymentStatusesRoutes);
