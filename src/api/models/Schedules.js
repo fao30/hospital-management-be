20a842c95b1388f4b40891fd1cd880f8be117b56
fao30 <fakhrularifin@windowslive.com>
2024-01-06 16:04:32 +0700

    Schedule add

---
+++ src/api/models/Schedules.js
@@ -0,0 +1,90 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Schedules extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // HERE define association here
+      Schedules.hasMany(models.Users, {
+        foreignKey: "doctor_id",
+        as: "doctor",
+      });
+      Schedules.hasMany(models.Users, {
+        foreignKey: "patient_id",
+        as: "patient",
+      });
+      Schedules.hasMany(models.Users, {
+        foreignKey: "admin_id",
+        as: "admin",
+      });
+    }
+  }
+  Schedules.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      doctor_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: "Users",
+          },
+          key: "id",
+        },
+      },
+      patient_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: {
+            tableName: "Users",
+          },
+          key: "id",
+        },
+      },
+      admin_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: {
+            tableName: "Users",
+          },
+          key: "id",
+        },
+      },
+      is_admin_approved: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      is_doctor_approved: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      date_time: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: new Date(),
+      },
+      status: {
+        type: DataTypes.ENUM,
+        values: ["SCHEDULED", "NOT_SHOW", "DONE", "CANCELLED"],
+      },
+    },
+    {
+      sequelize,
+      modelName: "Schedules",
+    }
+  );
+  return Schedules;
+};
