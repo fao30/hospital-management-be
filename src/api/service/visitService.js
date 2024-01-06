cfd03052c844821a5fc7603ec49806ed58513785
fao30 <fakhrularifin@windowslive.com>
2024-01-06 19:34:10 +0700

    Visit page

--- src/api/service/visitService.js
+++ src/api/service/visitService.js
@@ -11,19 +11,30 @@ class VisitsService {
     return Visits.create(query);
   }
 
-  static async findAllVisits(req) {
+  static async findAllVisits(limit, page, req = null) {
     const { hospital_id, role_id } = req.headers;
+    const created_at = req?.query.created_at;
+
+    let order = [["createdAt", "ASC"]];
+
     let where = {};
 
     if (role_id !== SUPER_ADMIN) {
       where.hospital_id = hospital_id;
     }
 
-    return Visits.findAll({
+    if (created_at) {
+      order = [["createdAt", created_at]];
+    }
+
+    return Visits.findAndCountAll({
       where,
+      limit,
+      offset: limit * page,
       attributes: {
         exclude: ["createdAt", "updatedAt"],
       },
+      order,
       include: [
         {
           model: Treatments,
