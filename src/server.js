0bab8ea4be7f0f4539fefc4e6bd8e8e82acd1e9f
fao30 <fakhrularifin@windowslive.com>
2024-01-09 12:20:44 +0700

    cors implmented

--- src/server.js
+++ src/server.js
@@ -24,13 +24,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const allowedOrigins = `${process.env.ALLOWED_ORIGINS}`.split(",");
 
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: allowedOrigins,
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(morgan("dev"));
 
