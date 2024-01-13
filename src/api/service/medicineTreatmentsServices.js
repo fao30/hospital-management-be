0136832405a9ee9bfffe6832078243976c5e7f4f
fao30 <fakhrularifin@windowslive.com>
2024-01-13 15:49:30 +0700

    decreasing ammount of medicine

--- src/api/service/medicineTreatmentsServices.js
+++ src/api/service/medicineTreatmentsServices.js
@@ -1,8 +1,40 @@
-const { Medicines_Treatments, Sequelize } = require("../models");
+const { BAD_REQUEST } = require("../constants/statusCode");
+const AppError = require("../helpers/AppError");
+const { Medicines_Treatments, Medicines, sequelize } = require("../models");
 
 class medicineTreatmentsService {
   static async createMedicineTreatment(query) {
-    return Medicines_Treatments.create(query);
+    let transaction;
+    try {
+      transaction = await sequelize.transaction();
+
+      const resp_medicine = await Medicines.findOne({
+        where: { id: query.medicine_id },
+        transaction,
+      });
+
+      const newStockAmount = resp_medicine.in_stock - query.quantity;
+      if (newStockAmount < 0) {
+        throw new AppError(BAD_REQUEST, "Medicine is not enough", 400);
+      }
+
+      const resp = await Medicines_Treatments.create(query, { transaction });
+
+      await Medicines.update(
+        { in_stock: newStockAmount },
+        {
+          where: { id: query.medicine_id },
+          transaction,
+        }
+      );
+
+      await transaction.commit();
+
+      return resp;
+    } catch (error) {
+      if (transaction) await transaction.rollback();
+      throw error;
+    }
   }
 
   static async findAllMedicineTreatments() {
